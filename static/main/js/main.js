01dd0947b57d34d73fff133f3f295e306f202973
siphesihlemagazi <siphemagazi@outlook.com>
2021-11-07 16:21:40 +0200

    Changed frontend design

--- static/main/js/main.js
+++ static/main/js/main.js
@@ -1,45 +1,19 @@
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
-const navLinks = document.getElementById("menuLink").querySelectorAll("li");
-var bars = document.getElementById("bars");
+const navLinks = document.querySelector('.navLinks');
+const closeMenu = document.getElementById('closeMenu');
+const openMenu = document.getElementById('openMenu');
+
+openMenu.addEventListener('click', open);
+closeMenu.addEventListener('click', close);
 
 
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
-// To make the nav close when a nav link is clicked
-for (let i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener('click',close);
-  }
-
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-    bars.style.display = "none";
+function open(){
+    navLinks.style.display = 'flex';
+    openMenu.style.display = 'none';
+    closeMenu.style.display = 'flex';
 }
+
 function close(){
-    mainMenu.style.top = '-300%';
-    bars.style.display = "block";
+    navLinks.style.display = '';
+    openMenu.style.display = 'flex';
+    closeMenu.style.display = 'none';
 }
-
-
-// Active nav link
-
-let menuLinks = document.getElementById('menuLink').querySelectorAll('a');
-menuLinks.forEach(a => {
-    a.addEventListener('click', function () {
-        menuLinks.forEach(link => link.classList.remove('active'));
-        this.classList.add('active')
-    })
-})
-
-
-//  Active pagination link
-
-let paginations = document.getElementById('pagination').querySelectorAll('a');
-paginations.forEach(a => {
-    a.addEventListener('click', function () {
-        paginations.forEach(link => link.classList.remove('active-pagination'));
-        this.classList.add('active-pagination')
-    })
-})
